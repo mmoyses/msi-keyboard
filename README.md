1090686dedfbced709a4898cf29bdac9e28a7ef7
Marcus Moyses <marcus.moyses@gmail.com>
2016-02-06 13:16:42 -0500

    msi keyboard led config

---
+++ README.md
@@ -0,0 +1,5 @@
+sudo npm install nave -g
+
+nave use 0.12.7 npm install msi-keyboard
+
+sudo nave use 0.12.7 node index
---
+++ index.js
@@ -0,0 +1,5 @@
+var keyboard = require('msi-keyboard');
+
+keyboard.color('left', { color: 'green', intensity: 'med' });
+keyboard.color('middle', { color: 'blue', intensity: 'med' });
+keyboard.color('right', { color: 'purple', intensity: 'med' });
