c999f57a69c59e56b417c7ae44bac7b9030aa1be
Marcus Moyses <marcus.moyses@gmail.com>
2016-02-06 18:10:43 -0500

    adjusting colors

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var keyboard = require('msi-keyboard');
 
-keyboard.color('left', { color: 'green', intensity: 'med' });
-keyboard.color('middle', { color: 'blue', intensity: 'med' });
+keyboard.color('left', { color: 'green', intensity: 'high' });
+keyboard.color('middle', { color: 'blue', intensity: 'low' });
 keyboard.color('right', { color: 'purple', intensity: 'med' });
