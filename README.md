040cb45bd5d06d53f1b69bd2c80b88f7c7eff7bf
Marcus Moyses <marcus.moyses@gmail.com>
2016-12-20 09:27:59 -0200

    new version

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 sudo npm install nave -g
 
-nave use 0.12.7 npm install msi-keyboard
+sudo dnf install libusb-devel gcc-c++
+
+nave use 0.12.7 npm install msi-keyboard rimraf
 
 sudo nave use 0.12.7 node index
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var keyboard = require('msi-keyboard');
+var keyboard = require('msi-keyboard')();
 
 keyboard.color('left', { color: 'green', intensity: 'high' });
 keyboard.color('middle', { color: 'purple', intensity: 'med' });
